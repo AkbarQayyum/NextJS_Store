a7afbc9b6fa561b55b58cb4be04176cb1c5ae2ec
AkbarQayyum <akbarqayyum3@gmail.com>
2022-08-31 11:15:30 +0500

    next js store

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
-import { useSelector } from 'react-redux'
-import React from 'react'
+import React from "react";
+import Card from "../Components/Card";
+import { Box } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import { useState } from "react";
 
-export default function Home() {
-  
+export const getStaticProps = async () => {
+  let res = await fetch(
+    "https://fakse-store-api.herokuapp.com/api/v1/products?limit=100&offset=0"
+  );
+  let data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ data }) {
   return (
-    <div>index</div>
-  )
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {data.map((m, i) => {
+          return <Card key={i} d={m} />;
+        })}
+      </Box>
+    </>
+  );
 }
